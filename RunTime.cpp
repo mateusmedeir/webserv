42b76bff032aba6dba0fdf3ae73b9587c9718202
PradoAllan <allanprado.it@gmail.com>
2025-09-08 08:44:52 -0300

    feat: :sparkles: init new runtime class

--- RunTime.cpp
+++ RunTime.cpp
@@ -1,20 +1,46 @@
 #include "RunTime.hpp"
 
-        // RunTime(void);
+// RunTime(void);
 RunTime::RunTime(void) {}
 
-        // ~RunTime(void);
+// ~RunTime(void);
 RunTime::~RunTime(void) {}
 
-        // RunTime(const RunTime &src);
+// RunTime(const RunTime &src);
 RunTime::RunTime(const RunTime &src) {
     *this = src;
 }
-        // RunTime &operator=(const RunTime &src);
+// RunTime &operator=(const RunTime &src);
 RunTime &RunTime::operator=(const RunTime &src) {
     if (this != &src) {
-        this->serverFd = src.serverFd;
-        this->addr = src.addr;
+        this->_serverFd = src._serverFd;
+        this->_serverAddr = src._serverAddr;
+        this->_epollInstance = src._epollInstance;
+        this->_configEpollEvents = src._configEpollEvents;
     }
     return (*this);
+}
+
+// RunTime(int socketDomain, int socketType, int serverPort, int serverAddr);
+RunTime::RunTime(int socketDomain, int socketType, int serverPort, int serverAddr) {
+    this->_serverFd = socket(socketDomain, socketType, 0);
+    if (this->_serverFd == -1) {
+        //error
+        //dar close nos FDs abertos
+    }
+    this->_serverAddr.sin_family = socketDomain;
+    this->_serverAddr.sin_port = htons(serverPort);
+    this->_serverAddr.sin_addr.s_addr = htons(serverAddr);
+    if (bind(this->_serverFd, (struct sockaddr *)&this->_serverAddr, sizeof(this->_serverAddr)) == -1) {
+        //error
+        //dar close nos FDs abertos
+    }
+    if (set_nonblocking(this->_serverFd) == -1) {
+        //error
+        //dar close nos FDs abertos
+    }
+    if (listen(this->_serverFd, MAX_EVENTS) == -1) {
+        //error
+        //dar close nos FDs abertos
+    }
 }
--- RunTime.hpp
+++ RunTime.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
 # include <netinet/in.h>
+# include <sys/epoll.h>
+# include "WebservHeader.hpp"
 
 class RunTime {
     private:
-        int     serverFd;
-        struct  sockaddr_in addr;
+        int                 _serverFd;
+        int                 _epollInstance;
+        struct sockaddr_in  _serverAddr;
+        struct epoll_event  _configEpollEvents;
+        struct epoll_event  _readyList[MAX_EVENTS];
+
     public:
         RunTime(void);
         ~RunTime(void);
         RunTime(const RunTime &src);
         RunTime &operator=(const RunTime &src);
+
+        RunTime(int socketDomain, int socketType, int serverPort, int serverAddr);
 };
