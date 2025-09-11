ab3a679adf63e688e271dda1ba29a4bb3e9d56f2
PradoAllan <allanprado.it@gmail.com>
2025-09-11 08:35:16 -0300

    refactor: :construction: add comments in the EpollInstance class.

--- EpollInstance.hpp
+++ EpollInstance.hpp
@@ -2,8 +2,13 @@
 
 # include "WebservHeader.hpp"
 
+// Acho que uma saida e declarar essa classe como Abstrata. Dessa forma, a classe RunTime
+// consegue acessar os atributos da Epoll diretamente, sem passar pelos metodos, e vai
+// conseguir implementar os metodos necessarios para manipular o Epoll tambem.
+// Preciso de ajuda para decidir isso. Mas acho que e uma boa saida.
+
 class EpollInstance {
-    private:
+    protected:
         int                 _epollFd;
         struct epoll_event  _configEpollEvents;
         struct epoll_event  _readyList[MAX_EVENTS];
@@ -12,11 +17,12 @@ class EpollInstance {
         EpollInstance(const EpollInstance &src);
         EpollInstance &operator=(const EpollInstance &src);
 
-        ~EpollInstance(void);
+        virtual ~EpollInstance(void);
 
         int getEpollFd(void) const;
 
-        void manipulateEpollInstance(int operation, int socketFd);
+        virtual void manipulateEpollInstance(int operation, int socketFd) const = 0;
+        virtual void initEpollInstance(void) const = 0;
 
         class CannotInitEpollInstance : public std::exception {
             public:
